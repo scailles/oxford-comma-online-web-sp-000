edf9d4ad2e12c10d50bac4e5855bfa7db6c196bd
Shaina C <scailles23@gmail.com>
2020-12-27 09:29:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,7 @@
 def oxford_comma(array)
- array[0..-2].join(', ') + ", and " + array[-1]
+ 
  array[0].join + array[1...].join(', ') + ", and " + array[-1]
 end
+
+
+#array[0..-2].join(', ') + ", and " + array[-1]
